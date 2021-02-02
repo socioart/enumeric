9b44cded504ff9c33d8431b1e44442c4d2c20769
labocho <labocho@penguinlab.jp>
2021-02-02 16:09:28 +0900

    implement

--- lib/enumeric.rb
+++ lib/enumeric.rb
@@ -2,5 +2,38 @@ require "enumeric/version"
 
 module Enumeric
   class Error < StandardError; end
-  # Your code goes here...
+
+  module_function
+  # @param values [Hash<Symbol, Object>]
+  def define(values)
+    _values = values.each_with_object({}) {|(name, value), h|
+      raise Error, "`#{name.inspect}` is not a Symbol" unless name.is_a?(Symbol)
+      raise Error, "`#{value.inspect}` does not have `freeze` method" unless value.respond_to?(:freeze)
+
+      h[name] = value.freeze
+    }.freeze
+
+    m = Module.new do
+      @values = _values
+      @values.each do |k, v|
+        const_set(k, v)
+      end
+
+      class << self
+        def include?(value)
+          @values.values.include?(value)
+        end
+
+        def name(value)
+          @values.key(value)
+        end
+
+        def values
+          @values
+        end
+      end
+    end
+
+    m
+  end
 end
--- spec/enumeric_spec.rb
+++ spec/enumeric_spec.rb
@@ -3,7 +3,78 @@ RSpec.describe Enumeric do
     expect(Enumeric::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "value method" do
+    after do
+      ::Object.send(:remove_const, :Enum) if defined?(Enum)
+    end
+
+    it "returns value" do
+      obj = Object.new
+      enum = Enumeric.define(
+        ONE: 1,
+        TWO: 2,
+        OBJECT: obj,
+      )
+
+      expect(enum::ONE).to eq 1
+      expect(enum::TWO).to eq 2
+      expect(enum::OBJECT).to eq obj
+    end
+
+    it "can be used in pattern matching" do
+      obj = Object.new
+      Enum = Enumeric.define(
+        ONE: 1,
+        TWO: 2,
+      )
+
+      res = case 1
+      in Enum::ONE
+        1
+      in Enum::TWO
+        2
+      end
+
+      expect(res).to eq 1
+
+      expect {
+        case 3
+        in Enum::ONE
+          1
+        in Enum::TWO
+          2
+        end
+      }.to raise_error(NoMatchingPatternError)
+    end
+  end
+
+  describe "include?" do
+    it "returns boolean" do
+      obj = Object.new
+      Enum = Enumeric.define(
+        ONE: 1,
+        TWO: 2,
+        OBJECT: obj,
+      )
+
+      expect(Enum.include?(1)).to eq true
+      expect(Enum.include?(3)).to eq false
+      expect(Enum.include?(obj)).to eq true
+    end
+  end
+
+  describe "name" do
+    it "returns Symbol or nil" do
+      obj = Object.new
+      Enum = Enumeric.define(
+        ONE: 1,
+        TWO: 2,
+        OBJECT: obj,
+      )
+
+      expect(Enum.name(1)).to eq :ONE
+      expect(Enum.name(3)).to eq nil
+      expect(Enum.name(obj)).to eq :OBJECT
+    end
   end
 end
